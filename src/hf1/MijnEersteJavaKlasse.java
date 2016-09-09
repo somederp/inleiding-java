df6fddf31b4cde7380e1a5d58d1d243b2df94b14
somederp <patrick.de.veen@gmail.com>
2016-09-09 13:35:59 +0200

    commit numero dos

---
+++ src/hf1/MijnEersteJavaKlasse.java
@@ -0,0 +1,11 @@
+package hf1;
+
+/**
+ * Created by Patri on 9-9-2016.
+ */
+public class MijnEersteJavaKlasse {
+
+    public static void main(String[] args) {
+        System.out.println("i did it");
+    }
+}
